12fe3f425dc45e25217042200440844b54f66c22
Yuji Hayashi <arthuryuji@hotmail.com>
2014-11-03 07:08:55 -0300

    Diagramando

--- wp-content/themes/greco/header.php
+++ wp-content/themes/greco/header.php
@@ -27,39 +27,74 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/library/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/fonts/stylesheet.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/images/icon.css">
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/site.css">
 	<?php wp_head(); ?>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/library/jquery-1.9.1.min.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/library/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/library/easing/jquery.easing.1.3.js"></script>
+	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/bootstrap_site.js"></script>
 </head>
-
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<?php if ( get_header_image() ) : ?>
-	<div id="site-header">
-		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-			<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
-		</a>
-	</div>
-	<?php endif; ?>
+	<div id="page" class="hfeed site">
+		<div class="extra-header">
+			<div class="site-header">
+				<div class="line-1 clearfix">
+					<div class="container-fluid">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".menu-principal">
+							<span class="sr-only">Navegação</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<form class="navbar-form navbar-right" role="search">
+							<div class="form-group">
+								<input type="text" class="form-control input-sm" placeholder="Search">
+							</div>
+							<button type="submit" class="button"><span class="icon-mag"></span><span class="sr-only">Pesquisar</span></button>
+						</form>
+						<div class="header-social">
+							<a href="#" target="_blank" title="Acesse nosso Facebook"><span class="icon-facebook"></span><span class="sr-only">Facebook</span></a>
+							<a href="#" target="_blank" title="Acesse nosso Instagram"><span class="icon-instagram"></span><span class="sr-only">Instagram</span></a>
+							<a href="#" target="_blank" title="Acesse nosso Twitter"><span class="icon-twitter"></span><span class="sr-only">Twitter</span></a>
+						</div> <!-- .header-social -->
+					</div> <!-- .container-fluid -->
+				</div> <!-- .line-1 -->
+				<nav class="navbar navbar-default" role="navigation">
+					<div class="container">
+						<div class="navbar-header">
+							<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="Ir para a página inicial"><img src="<?php echo get_template_directory_uri(); ?>/images/greco-forma.png" alt="Logotipo <?php bloginfo( 'name' ); ?>"></a>
+						</div>
+						<div class="collapse navbar-collapse menu-principal">
+							<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav navbar-nav', 'link_before' => '', 'link_after' => ' <span class="icon-arrow-down icone"></span>', 'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>' ) ); ?>
+						</div><!-- /.navbar-collapse -->
+					</div> <!-- .container -->
+				</nav>
+			</div> <!-- .site-header -->
+		</div> <!-- .extra-header -->
 
-	<header id="masthead" class="site-header" role="banner">
-		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-			<div class="search-toggle">
-				<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
+		<header id="masthead" class="site-header" role="banner">
+			<div class="header-main">
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<div class="search-toggle">
+					<a href="#search-container" class="screen-reader-text"><?php _e( 'Search', 'twentyfourteen' ); ?></a>
+				</div>
+
+				<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
+					<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
+					<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+				</nav>
 			</div>
 
-			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">
-				<button class="menu-toggle"><?php _e( 'Primary Menu', 'twentyfourteen' ); ?></button>
-				<a class="screen-reader-text skip-link" href="#content"><?php _e( 'Skip to content', 'twentyfourteen' ); ?></a>
-				<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-			</nav>
-		</div>
-
-		<div id="search-container" class="search-box-wrapper hide">
-			<div class="search-box">
-				<?php get_search_form(); ?>
+			<div id="search-container" class="search-box-wrapper hide">
+				<div class="search-box">
+					<?php get_search_form(); ?>
+				</div>
 			</div>
-		</div>
-	</header><!-- #masthead -->
+		</header><!-- #masthead -->
 
-	<div id="main" class="site-main">
+		<div id="main" class="site-main">
